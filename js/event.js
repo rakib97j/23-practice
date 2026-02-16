b4f766ffe2663ca7b7ab8dc79dab87726fb4394e
rakib97j <rakibhassan23012007@gmail.com>
2026-02-17 01:52:34 +0600

    push

--- js/event.js
+++ js/event.js
@@ -1,15 +1,28 @@
 console.log("check");
-// option 2 add event handle 
+// option 2 add event handle
 function makeWhite() {
   document.body.style.backgroundColor = "white";
 }
 function makeBlack() {
   document.body.style.backgroundColor = "black";
 }
-function makeBlue(){
-    document.body.style.backgroundColor ="blue"
+function makeBlue() {
+  document.body.style.backgroundColor = "blue";
 }
 
-function font (){
-    document.body.style.backgroundColor ="green"
+function font() {
+  document.body.style.backgroundColor = "green";
+}
+// 3rd option
+const blueBtn = document.getElementById("blue-btn");
+blueBtn.onclick = function clickOn() {
+  document.body.style.backgroundColor = "purple";
+};
+// 3rd option
+const makeGray = document.getElementById("gray");
+
+makeGray.onclick =clickGray;
+
+function clickGray(){
+    document.body.style.backgroundColor =('gray')
 }
