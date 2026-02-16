899bb032393accfb767fe2dfd70254b801d4ef08
rakib97j <rakibhassan23012007@gmail.com>
2026-02-17 01:27:17 +0600

    push

--- js/event.js
+++ js/event.js
@@ -1,8 +1,15 @@
 console.log("check");
-
+// option 2 add event handle 
 function makeWhite() {
   document.body.style.backgroundColor = "white";
 }
 function makeBlack() {
   document.body.style.backgroundColor = "black";
+}
+function makeBlue(){
+    document.body.style.backgroundColor ="blue"
+}
+
+function font (){
+    document.body.style.backgroundColor ="green"
 }
