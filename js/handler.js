076c307c55465e5addec91aa254b13ec758b0091
rakib97j <rakibhassan23012007@gmail.com>
2026-02-18 01:27:53 +0600

    video no.4

--- js/handler.js
+++ js/handler.js
@@ -1,1 +1,33 @@
-console.log("connect");
+// banner title change
+
+document.getElementById("update-text").addEventListener("click", function () {
+  // console.log("clicker");
+  const textChange = document.getElementById("text");
+  textChange.innerText = "Updated Text by event listener";
+
+  document.body.style.backgroundColor = "gray";
+});
+
+// login 2nd section event listener
+
+document.getElementById("login-btn").addEventListener("click", function () {
+  const changeText = document.getElementById("title");
+  changeText.innerText = "successfully login";
+
+  const btnChange = document.getElementById("login-btn");
+  btnChange.innerText = "GO TO HOME >>>>";
+});
+
+
+
+//  <!-- name change by input from user  -->
+
+document.getElementById("input-btn").addEventListener("click", function () {
+  const inputText = document.getElementById("input-text");
+  // console.log('input name');
+  const name = inputText.value;
+  // console.log('name' ,name);
+
+  const mainName = document.getElementById("main-name");
+  mainName.innerText = name;
+});
