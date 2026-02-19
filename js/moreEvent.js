7744e447927f2a50d48251e0fd6af98e43a6c70b
rakib97j <rakibhassan23012007@gmail.com>
2026-02-19 14:13:18 +0600

    more event type Mouseenter ,mouseout ,mousemove ,focus &blur event keyup key down event

---
+++ js/moreEvent.js
@@ -0,0 +1,39 @@
+// // MOUSE ENTER EVENT
+// document.getElementById('mouse-enter').addEventListener('mouseenter' ,function(){
+//     document.body.style.backgroundColor="red"
+//     console.log('mouse enter event');
+// })
+
+// // MOUSE MOVE EVENT
+// document.getElementById('mouse-enter').addEventListener('mousemove' ,function(){
+//     document.body.style.backgroundColor="red"
+//     console.log('mouse enter event');
+// })
+
+// //  MOUSE OUT EVENT
+//   document
+//     .getElementById("mouse-enter")
+//     .addEventListener("mouseout", function () {
+//       document.body.style.backgroundColor = "red";
+//       console.log("mouse enter event");
+//     })
+
+// document
+//   .getElementById("text-input")
+//   .addEventListener("mousemove", function () {
+//     console.log("focus the input type name");
+//   });
+// // focus event
+// document.getElementById("email-input").addEventListener("focus", function () {
+//   console.log("focus the input type email");
+// });
+
+// // BLUR EVENT
+
+// document.getElementById("text-input").addEventListener("blur", function () {
+//   console.log("focus the input type name Doneeeeeeeeeeeee");
+// });
+// // focus event
+// document.getElementById("email-input").addEventListener("blur", function () {
+//   console.log("focus the input type email Doneeeeeeeeeeee");
+// });
