3aeb2daa770fd7499a4ada155146f658246776b1
rakib97j <rakibhassan23012007@gmail.com>
2026-02-19 13:35:21 +0600

    comment box

--- js/commentBox.js
+++ js/commentBox.js
@@ -1,1 +1,16 @@
-
+document.getElementById("comment-btn").addEventListener("click", function () {
+  //   1 add text area
+  const textBox = document.getElementById("text-box");
+  //   2 set the value of text area
+  const newComment = textBox.value;
+  //   3  submit destination
+  const publishComment = document.getElementById("comment-box");
+  //   4 create
+  const newP = document.createElement("p");
+  newP.classList.add("comment");
+  newP.innerText = newComment;
+  //   5 append p
+  publishComment.appendChild(newP);
+  //   6 clean
+  textBox.value = "";
+});
